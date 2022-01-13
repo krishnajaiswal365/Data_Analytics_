2d0dafbcc5772e98e4bf4f0ef166e0d2cda50c42
krishnajaiswal365 <96965563+krishnajaiswal365@users.noreply.github.com>
2022-01-14 00:05:00 +0530

    for loop use
    when i was child then i was write 2 to 15 table

---
+++ table2.py
@@ -0,0 +1,9 @@
+print("\t\t\t\t\t\t Multiplication \n")
+for i in range(1,16):
+    print(i,end="\t")
+print()
+print("__________________________________________________________________________________________________________________\n")
+for j in range(1,16):
+    for k in range(1,16):
+        print(j*k,end="\t")
+    print("\n")
